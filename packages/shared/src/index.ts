5605a871a7bcb1d3d454b541296d73b3179c1a90
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2026-04-01 21:33:27 +0200

    chore: fix ts autocomplete/intellisense

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,1 +1,5 @@
-export * from './utils';
+export * from './utils/utils';
+export const bezi = () => 'bezi';
+export const moh = () => 'mohamed';
+export const omar = () => 'omar';
+export const nour = () => 'nour';
