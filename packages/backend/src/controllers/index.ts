d804f7068693e35a456457b7521e6b795136a4fe
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2026-04-25 16:36:09 +0300

    refactor: remove unused controller, service, persistence, and router exports

--- packages/backend/src/controllers/index.ts
+++
@@ -1,5 +0,0 @@
-import UserController from './user-controller';
-import PostController from './post-controller';
-import MarketingController from './marketing-controller';
-
-export { UserController, PostController, MarketingController };
--- packages/backend/src/services/index.ts
+++
@@ -1,6 +0,0 @@
-import UserService from './user-service';
-import PostService from './post-service';
-import MarketingService from './marketing-service';
-import ContactListAPI from './contact-list-api';
-
-export { UserService, PostService, MarketingService, ContactListAPI };
