cb50516d9f882adc622bc616126bc8098709a3bb
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2026-05-03 17:37:48 +0300

    refactor(marketing): update return type of addEmailToList method

--- packages/backend/src/modules/marketing/marketing-service.ts
+++ packages/backend/src/modules/marketing/marketing-service.ts
@@ -1,10 +1,14 @@
 import { AddEmailToListDTO } from './marketing-dtos';
 import { ContactListAPI } from './contact-list-api';
 
+import { EmailSubscription } from '@dddforum/shared/api/marketing';
+
 export class MarketingService {
   constructor(private contactListAPI: ContactListAPI) {}
 
-  public async addEmailToList(dto: AddEmailToListDTO) {
+  public async addEmailToList(
+    dto: AddEmailToListDTO,
+  ): Promise<EmailSubscription> {
     const result = await this.contactListAPI.addEmailToList(
       dto.email,
     );
