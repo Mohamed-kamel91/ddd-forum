8677be90ffebb6b4b50b09dd037f6ac0e74293c3
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2026-05-11 20:27:41 +0300

    feat(registration): enhance RegistrationForm with input handling and validation

--- packages/frontend/src/components/registrationForm.tsx
+++ packages/frontend/src/components/registrationForm.tsx
@@ -1,72 +1,139 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { appSelectors, toClass } from '../shared/selectors';
 
 export type RegistrationInput = {
   email: string;
   username: string;
   firstName: string;
   lastName: string;
+  password: string;
+  allowMarketingEmails: boolean;
 };
 
-interface RegistrationFormProps {
+type RegistrationFormProps = {
   onSubmit: (formDetails: RegistrationInput) => void;
-}
+};
 
-export const RegistrationForm = (props: RegistrationFormProps) => {
-  const [email, setEmail] = useState('email');
-  const [username, setUsername] = useState('username');
-  const [firstName, setFirstName] = useState('firstName');
-  const [lastName, setLastName] = useState('lastName');
+export const RegistrationForm = ({
+  onSubmit,
+}: RegistrationFormProps) => {
+  const selectors = appSelectors.registration.registrationForm;
 
-  const handleSubmit = () => {
-    props.onSubmit({
+  const [input, setInput] = useState<RegistrationInput>({
+    email: '',
+    username: '',
+    firstName: '',
+    lastName: '',
+    password: '',
+    allowMarketingEmails: false,
+  });
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const { name, value, checked, type } = e.target;
+
+    setInput((prevInput) => {
+      return {
+        ...prevInput,
+        [name]: type === 'checkbox' ? checked : value,
+      };
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const {
       email,
       username,
       firstName,
       lastName,
+      password,
+      allowMarketingEmails,
+    } = input;
+
+    onSubmit({
+      email,
+      username,
+      firstName,
+      lastName,
+      password,
+      allowMarketingEmails,
     });
   };
 
   return (
-    <div className="registration-form">
-      <div>Create Account</div>
+    <form
+      className="registration-form"
+      onSubmit={handleSubmit}
+      noValidate
+    >
+      <h2>Create Account</h2>
       <input
-        className="registration email"
+        className={toClass(selectors.email.selector)}
         type="email"
-        placeholder="email"
-        onChange={(e) => setEmail(e.target.value)}
-      ></input>
+        name="email"
+        value={input.email}
+        placeholder="Email"
+        onChange={handleInputChange}
+      />
       <input
-        className="registatation-input username"
+        className={toClass(selectors.firstName.selector)}
         type="text"
-        placeholder="username"
-        onChange={(e) => setUsername(e.target.value)}
-      ></input>
+        name="firstName"
+        value={input.firstName}
+        placeholder="First name"
+        onChange={handleInputChange}
+      />
       <input
-        className="registatation-input username"
+        className={toClass(selectors.lastName.selector)}
         type="text"
-        placeholder="first name"
-        onChange={(e) => setFirstName(e.target.value)}
-      ></input>
+        name="lastName"
+        value={input.lastName}
+        placeholder="Last name"
+        onChange={handleInputChange}
+      />
       <input
-        className="registatation-input username"
+        className={toClass(selectors.username.selector)}
         type="text"
-        placeholder="last name"
-        onChange={(e) => setLastName(e.target.value)}
-      ></input>
+        name="username"
+        value={input.username}
+        placeholder="Username"
+        onChange={handleInputChange}
+      />
+      <input
+        className={toClass(selectors.password.selector)}
+        type="text"
+        name="password"
+        value={input.password}
+        placeholder="Password"
+        onChange={handleInputChange}
+      />
+      <label className="registration-label marketing-emails-label">
+        <input
+          className={toClass(selectors.marketingCheckbox.selector)}
+          type="checkbox"
+          name="allowMarketingEmails"
+          checked={input.allowMarketingEmails}
+          onChange={handleInputChange}
+        />
+        Want to be notified about event & discounts?
+      </label>
+
       <div>
-        <div className="to-login">
-          <div>Already have an account?</div>
-          <Link to="/login">Login</Link>
-        </div>
         <button
-          onClick={() => handleSubmit()}
-          className="submit-button"
+          className={toClass(selectors.submit.selector)}
           type="submit"
         >
           Submit
         </button>
+        <div className="to-login">
+          <div>Already have an account?</div>
+          <Link to="/login">Login</Link>
+        </div>
       </div>
-    </div>
+    </form>
   );
 };
