f60c846e4fcfb697a8be022ce108d5e8a33aa36f
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2026-05-11 20:06:40 +0300

    chore: change CreateUserBuilder  in shared package test folder

--- packages/shared/tests/support/builders/create-user-builder.ts
+++ packages/shared/tests/support/builders/create-user-builder.ts
@@ -1,5 +1,5 @@
 import { faker } from '@faker-js/faker';
-import { type CreateUserInput } from '../../src/api/user';
+import { CreateUserInput } from '../../../src/api/user';
 
 export class CreateUserBuilder {
   private props: CreateUserInput;
