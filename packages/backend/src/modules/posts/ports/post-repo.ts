cb1d35369c8c5630c76ca8fdbd6b9dc2aa118360
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2026-05-20 19:32:59 +0300

    refacor(posts): update IPostRepo to use getPostsQuery for getAll method

--- packages/backend/src/modules/posts/ports/post-repo.ts
+++ packages/backend/src/modules/posts/ports/post-repo.ts
@@ -1,6 +1,6 @@
-import { type Post } from '@dddforum/shared/api/post';
-import { getPostsDTO } from '../post-query';
+import type { Post } from '@dddforum/shared/api/post';
+import type { getPostsQuery } from '../post-query';
 
 export interface IPostRepo {
-  getAll: (dto: getPostsDTO) => Promise<Post[]>;
+  getAll: (dto: getPostsQuery) => Promise<Post[]>;
 }
