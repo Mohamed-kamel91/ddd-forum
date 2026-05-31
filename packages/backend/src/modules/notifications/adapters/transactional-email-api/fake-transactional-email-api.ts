14cb222837e981931295c3b14ebaec1bfc0948ad
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2026-05-31 19:00:15 +0300

    test(fakes): extend FakeTransactionalEmailAPI with Spy call tracking

--- packages/backend/src/modules/notifications/adapters/transactional-email-api/fake-transactional-email-api.ts
+++ packages/backend/src/modules/notifications/adapters/transactional-email-api/fake-transactional-email-api.ts
@@ -1,10 +1,19 @@
+import { Spy } from '../../../../shared/test-doubles/spy';
 import type {
   ITransactionalEmailAPI,
   SendMailInput,
 } from '../../ports/transactional-email-api';
 
-export class FakeTransactionalEmailAPI implements ITransactionalEmailAPI {
-  public async sendMail(_: SendMailInput): Promise<boolean> {
+export class FakeTransactionalEmailAPI
+  extends Spy<ITransactionalEmailAPI>
+  implements ITransactionalEmailAPI
+{
+  constructor() {
+    super();
+  }
+
+  public async sendMail(input: SendMailInput): Promise<boolean> {
+    this.addCall('sendMail', [input]);
     return true;
   }
 }
